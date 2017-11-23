e57304f6aebb32abcddf8c8b7a3d9f97fdcac8e4
Buithienbao <thienbao_bui@yahoo.com>
2017-11-23 09:39:54 +0700

    add to cart function

--- view/js/myscript.js
+++ view/js/myscript.js
@@ -27,48 +27,71 @@ var app = window.app || {},
     };
 
     app.createProducts = function () {
-        
-      
-        
-        var productos = [
-            {
-                id: 1,
-                img: 'view/images/robot.jpg',
-                name: 'Robot',
-                price: 299.00,
-                desc: 'Libertad 5oz BU 1998 Contains 1 Libertad 5oz BU brilliant uncirculated .999 fine Silver. In capsule The same coin as you see in this picture. We only Ship to the US, and is FREE Shipping Shipping time 5-7 business days via UPS express with tracking and insurance. Payments only via Paypal.',
-                stock: 4
-            },
-            {
-                id: 2,
-                name: 'Keyboard',
-                img: 'view/images/keyboard.jpg',
-                price: 199.00,
-                desc: 'Libertad 5oz BU 1998 Contains 1 Libertad 5oz BU brilliant uncirculated .999 fine Silver. In capsule The same coin as you see in this picture. We only Ship to the US, and is FREE Shipping Shipping time 5-7 business days via UPS express with tracking and insurance. Payments only via Paypal.',
-                stock: 2
-            },
-            {
-                id: 3,
-                name: 'Headphone',
-                img: 'view/images/headphone.jpg',
-                price: 99.00,
-                desc: 'Libertad 5oz BU 1998 Contains 1 Libertad 5oz BU brilliant uncirculated .999 fine Silver. In capsule The same coin as you see in this picture. We only Ship to the US, and is FREE Shipping Shipping time 5-7 business days via UPS express with tracking and insurance. Payments only via Paypal.',
-                stock: 1
-            },
-            {
-                id: 4,
-                name: 'Libertad 5oz',
-                img: 'view/images/headphone.jpg',
-                price: 80.00,
-                desc: 'Libertad 5oz BU 1998 Contains 1 Libertad 5oz BU brilliant uncirculated .999 fine Silver. In capsule The same coin as you see in this picture. We only Ship to the US, and is FREE Shipping Shipping time 5-7 business days via UPS express with tracking and insurance. Payments only via Paypal.',
-                stock: 2
-            }
-        ],
-                wrapper = $('.productosWrapper'),
-                contenido = '';
-        for (var i = 0; i < productos.length; i++) {
+        var products;
 
-            if (productos[i].stock > 0) {
+//        
+//        var productos = [
+//            {
+//                id: 1,
+//                img: 'view/images/robot.jpg',
+//                name: 'Robot',
+//                price: 299.00,
+//                desc: 'Libertad 5oz BU 1998 Contains 1 Libertad 5oz BU brilliant uncirculated .999 fine Silver. In capsule The same coin as you see in this picture. We only Ship to the US, and is FREE Shipping Shipping time 5-7 business days via UPS express with tracking and insurance. Payments only via Paypal.',
+//                stock: 4
+//            },
+//            {
+//                id: 2,
+//                name: 'Keyboard',
+//                img: 'view/images/keyboard.jpg',
+//                price: 199.00,
+//                desc: 'Libertad 5oz BU 1998 Contains 1 Libertad 5oz BU brilliant uncirculated .999 fine Silver. In capsule The same coin as you see in this picture. We only Ship to the US, and is FREE Shipping Shipping time 5-7 business days via UPS express with tracking and insurance. Payments only via Paypal.',
+//                stock: 2
+//            },
+//            {
+//                id: 3,
+//                name: 'Headphone',
+//                img: 'view/images/headphone.jpg',
+//                price: 99.00,
+//                desc: 'Libertad 5oz BU 1998 Contains 1 Libertad 5oz BU brilliant uncirculated .999 fine Silver. In capsule The same coin as you see in this picture. We only Ship to the US, and is FREE Shipping Shipping time 5-7 business days via UPS express with tracking and insurance. Payments only via Paypal.',
+//                stock: 1
+//            },
+//            {
+//                id: 4,
+//                name: 'Libertad 5oz',
+//                img: 'view/images/headphone.jpg',
+//                price: 80.00,
+//                desc: 'Libertad 5oz BU 1998 Contains 1 Libertad 5oz BU brilliant uncirculated .999 fine Silver. In capsule The same coin as you see in this picture. We only Ship to the US, and is FREE Shipping Shipping time 5-7 business days via UPS express with tracking and insurance. Payments only via Paypal.',
+//                stock: 2
+//            }
+//        ],
+//                wrapper = $('.productosWrapper'),
+//                contenido = '';
+//        for (var i = 0; i < productos.length; i++) {
+//
+//            if (productos[i].stock > 0) {
+//
+//                contenido += '<div class="coin-wrapper">';
+//                contenido += '		<img src="' + productos[i].img + '" alt="' + productos[i].name + '">';
+//                contenido += '		<span class="large-12 columns product-details">';
+//                contenido += '			<h3>' + productos[i].name + ' <span class="price">$ ' + productos[i].price + ' USD</span></h3>';
+//                contenido += '			<h3>Quality: <span class="stock">' + productos[i].stock + '</span></h3>';
+//                contenido += '		</span>';
+//                contenido += '		<a class="large-12 columns btn submit ladda-button prod-' + productos[i].id + '" data-style="slide-right" onclick="app.addtoCart(' + productos[i].id + ');">Add to cart</a>';
+//                contenido += '		<div class="clearfix"></div>';
+//                contenido += '</div>';
+//
+//            }
+//
+//        }
+        $.ajax({
+            type: "POST",
+            url : "controller/AjaxGetProduct.php",
+            success: function(response){
+                var productos = JSON.parse(response),
+                  wrapper = $('.productosWrapper'),
+                    contenido='';
+                for (var i = 0; i < productos.length; i++) {
+
 
                 contenido += '<div class="coin-wrapper">';
                 contenido += '		<img src="' + productos[i].img + '" alt="' + productos[i].name + '">';
@@ -80,48 +103,27 @@ var app = window.app || {},
                 contenido += '		<div class="clearfix"></div>';
                 contenido += '</div>';
 
-            }
-
-        }
-        $.ajax({
-            type: "POST",
-            url : "controller/AjaxGetProduct.php",
-            success: function(response){
-                console.log(JSON.parse(response));
-                var product = JSON.parse(response);
-                for (var i = 0; i < product.length; i++) {
-
-
-                contenido += '<div class="coin-wrapper">';
-                contenido += '		<img src="' + product[i].img + '" alt="' + product[i].name + '">';
-                contenido += '		<span class="large-12 columns product-details">';
-                contenido += '			<h3>' + product[i].name + ' <span class="price">$ ' + product[i].price + ' USD</span></h3>';
-                contenido += '			<h3>Quality: <span class="stock">' + product[i].stock + '</span></h3>';
-                contenido += '		</span>';
-                contenido += '		<a class="large-12 columns btn submit ladda-button prod-' + product[i].id + '" data-style="slide-right" onclick="app.addtoCart(' + product[i].id + ');">Add to cart</a>';
-                contenido += '		<div class="clearfix"></div>';
-                contenido += '</div>';
-
             
 
                 }
-                
+                wrapper.html(contenido);
+                localStorage.setItem('productos', JSON.stringify(productos));   
             },
             error: function(){
                 console.log("Fail vcl");
             }
         });
-        wrapper.html(contenido);
+       
 
-        localStorage.setItem('productos', JSON.stringify(productos));
     };
 
     app.addtoCart = function (id) {
         var l = Ladda.create(document.querySelector('.prod-' + id));
-
+        console.log(JSON.parse(localStorage.getItem('productos')));
+        console.log(_.find(JSON.parse(localStorage.getItem('productos')), {'id': ""+id+""}));
         l.start();
         var productos = JSON.parse(localStorage.getItem('productos')),
-                producto = _.find(productos, {'id': id}),
+                producto = _.find(productos, {'id': ""+id+""}),
                 cant = 1;
         if (cant <= producto.stock) {
             if (undefined !== producto) {
@@ -144,7 +146,7 @@ var app = window.app || {},
 
     app.searchProd = function (cart, id, cant, name, price, img, available) {
         //si le pasamos un valor negativo a la cantidad, se descuenta del carrito
-        var curProd = _.find(cart.items, {'id': id});
+        var curProd = _.find(cart.items, {'id': ""+id+""});
 
         if (undefined !== curProd && curProd !== null) {
             //ya existe el producto, aÃ±adimos uno mÃ¡s a su cantidad
@@ -204,7 +206,7 @@ var app = window.app || {},
     app.updateItem = function (id, available) {
         //resta uno a la cantidad del carrito de compras
         var cart = (JSON.parse(localStorage.getItem('cart')) !== null) ? JSON.parse(localStorage.getItem('cart')) : {items: []},
-                curProd = _.find(cart.items, {'id': id});
+                curProd = _.find(cart.items, {'id': ""+id+""});
         //actualizar el carrito
         curProd.cant = curProd.cant - 1;
         //validar que la cantidad no sea menor a 0
@@ -220,7 +222,7 @@ var app = window.app || {},
 
     app.delete = function (id) {
         var cart = (JSON.parse(localStorage.getItem('cart')) !== null) ? JSON.parse(localStorage.getItem('cart')) : {items: []};
-        var curProd = _.find(cart.items, {'id': id});
+        var curProd = _.find(cart.items, {'id': ""+id+""});
         _.remove(cart.items, curProd);
         localStorage.setItem('cart', JSON.stringify(cart));
         app.init();
